8c83a70ae232cfded9d067255cec0541d251f253
RyuGeun <h46344@naver.com>
2022-05-23 15:27:57 +0900

    feat : 내용수정

--- src/main/java/com/dw/board/vo/BoardVO.java
+++ src/main/java/com/dw/board/vo/BoardVO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 public class BoardVO extends StudentsVO {
 	private int boardId;
 	private int studentsId;
+	
 	private String title;
 	private String content;
 	private String updateAt;
