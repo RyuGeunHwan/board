4cc86933f7e62f16d5cb7b47628d6373305db96e
RyuGeunHwan <h46344@naver.com>
2022-05-31 19:19:40 +0900

    feat : PageHandler 내용 수정

--- src/main/java/com/dw/board/utils/PageHandler.java
+++ src/main/java/com/dw/board/utils/PageHandler.java
@@ -29,6 +29,7 @@ public class PageHandler {
 		 *comment : 총 페이지 수
 		 */
 	public int calcPage(int total, int pageSize) {
+		// total : 총 게시글, pageSize : 한 페이지당 들어갈 게시글 수
 		int pages = total / pageSize;
 		if(total % pageSize > 0){
 			pages++;
@@ -47,6 +48,7 @@ public class PageHandler {
 		 *comment : 현재 페이지 블록 알아내기
 		 */
 	public void setNowBlock(int pageNum, int navigatePages) {
+		// pageNum : 현재 페이지, navigatePages : 한 블록에 보여질 페이지 수 ex) 1블록 => 1페이지~10페이지
 		this.nowBlock = pageNum / navigatePages;
 		if(pageNum % navigatePages > 0) {
 			this.nowBlock++;
@@ -61,8 +63,10 @@ public class PageHandler {
 		 * @date : 2022. 5. 31.
 		 *comment : 마지막 블록 알아내기
 		 */
-	public void setLastBlock(int total) {
+	public void setLastBlock(int total) { // total : 총 게시글 수
 		this.lastBlock = total / (this.navigatePages * this.pageSize);
+			// this.navigatePages : 필드변수에 있는 한 블록에 보여질 페이지 수, 
+			// this.pageSize : 필드변수에 있는 한 페이지에 보여질 게시글 수
 		if(total % (this.navigatePages * this.pageSize) > 0) {
 			this.lastBlock++;
 		}
@@ -77,7 +81,11 @@ public class PageHandler {
 		 * @date : 2022. 5. 31.
 		 *comment : 현재 블록의 첫 페이지
 		 */
-	public void setStartPage(int nowBlock) {
+	public void setStartPage(int nowBlock) { //nowBlock : 현재 해당되는 블록
+		// this.startPage : 현재 블록의 첫페이지 ex) 2블록 => 11페이지 ~ 20페이지
+		// ex) 한페이지당 게시글 5개씩 보여줄 수 있고 현재 페이지는 8페이지이다. 현재 블록은 2블록이다.
+		// this.startPage  = (2 * 5) - (5 - 1) == 6
+		// this.startPage = 6 => 해당 페이지 블록의 첫시작 페이지는 6페이지이다.
 		this.startPage = (nowBlock * this.navigatePages) - (this.navigatePages - 1);
 		
 	}
@@ -92,6 +100,7 @@ public class PageHandler {
 		 *comment : 현재 블록의 마지막 페이지
 		 */
 	public void setEndPage(int lastBlock, int nowBlock) {
+		// 한블록에 보여질 페이지수 : 
 		this.endPage = nowBlock * this.navigatePages;
 		if(nowBlock == this.lastBlock) {
 			this.endPage = total;
