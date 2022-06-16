bc65f7b313494dc9aa7542342d59b6fcf8332254
RyuGeunHwan <h46344@naver.com>
2022-06-16 22:36:27 +0900

    feat : 내용수정

--- src/main/java/com/dw/board/vo/LogVO.java
+++ src/main/java/com/dw/board/vo/LogVO.java
@@ -15,5 +15,51 @@ public class LogVO {
 	private String httpMethod;
 	private String createAt;
 	
+	
+	public int getLogId() {
+		return logId;
+	}
+	public void setLogId(int logId) {
+		this.logId = logId;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getHttpMethod() {
+		return httpMethod;
+	}
+	public void setHttpMethod(String httpMethod) {
+		this.httpMethod = httpMethod;
+	}
+	public String getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(String createAt) {
+		this.createAt = createAt;
+	}
+	
+	
+	
 
 }
